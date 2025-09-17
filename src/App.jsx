40b21edac5d52f9fd437755705e5adca2991c355
monirHRaju <monirhraju@gmail.com>
2025-09-17 11:02:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Count from './Count'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>Stat and Event Handling</h2>
+      <Count></Count>
+    </>
+  )
+}
+
+export default App
---
+++ src/Count.jsx
@@ -0,0 +1,13 @@
+export default function Count () {
+
+    return (
+      <div>
+        <div  className="counter">
+          <h1>Count 1</h1>
+        </div>
+        <button>+1</button>
+        <button>-1</button>
+        <button>Reset</button>
+      </div>
+    )
+}
