71f677848800d459e8a621dd923d1a1aa78b9dad
monirHRaju <monirhraju@gmail.com>
2025-09-17 15:06:16 +0600

    count state done.

--- src/Count.jsx
+++ src/Count.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react"
+
 export default function Count () {
 
-    return (
+  const [count, setCount] = useState(0)
+  const increaseCountHandler = () => {
+    setCount(count + 1)
+  }  
+  
+  const decreaseCountHandler = () => {
+    setCount(count - 1)
+  }  
+  
+  const resetCounter = () => {
+    setCount(0)
+  }  
+
+  return (
+
       <div>
         <div  className="counter">
-          <h1>Count 1</h1>
+          <h1>Count {count}</h1>
         </div>
-        <button>+1</button>
-        <button>-1</button>
-        <button>Reset</button>
+        <button onClick={increaseCountHandler}>+1</button>
+        <button onClick={decreaseCountHandler}>-1</button>
+        <button onClick={resetCounter}>Reset</button>
       </div>
     )
 }
